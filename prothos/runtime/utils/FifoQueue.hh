5c19c2a60bf9e460a9b9f772c54e91158846fde2
Philipp Gypser <gypsephi@b-tu.de>
2018-09-16 17:51:56 +0200

    FifoQueue

--- prothos/runtime/utils/FifoQueue.hh
+++ prothos/runtime/utils/FifoQueue.hh
@@ -1,32 +1,53 @@
 #pragma once
 
-#include <queue>
-#include "runtime/Mutex.hh"
+#include "runtime/mlog.hh"
+#include "util/assert.hh"
+#include <atomic>
 
+// single reader, multiple writer fifo
 template <typename T>
 class FifoQueue
 {
- public:
+	public:
+		FifoQueue()
+			: head(nullptr)
+			, tail(nullptr)
+		{}
 
-  T* pop() {
-	mythos::Mutex::Lock l(mutex);
-	if(queue.empty()) return nullptr;
-    auto item = queue.front();
-    queue.pop();
-    return item;
-  }
+		void push(T* e){
+			MLOG_INFO(mlog::app, __func__, DVAR(e), DVAR(this));
+			ASSERT(e);
+			auto h =head.load();
+			do{
+				e->next.store(h);
+			}while(!head.compare_exchange_weak(h,e));
+		}
 
-  void push(const T* item) {
-	mythos::Mutex::Lock l(mutex);
-    queue.push(item);
-  }
+		T* pop(){
+			T* ret = nullptr;
+			if(tail == nullptr){
+				auto h = head.exchange(nullptr);
 
-  int size(){
-	mythos::Mutex::Lock l(mutex);
-    return queue.size();
-  }
+				//invert list
+				while(h != nullptr){
+					auto tmp = h->next.load();
+					h->next = tail;
+					tail = h;
+					h = tmp;
+				}
+			}
+				
+			if(tail != nullptr){
+				MLOG_INFO(mlog::app, __func__, DVAR(tail), DVAR(this));
+				ret = tail;
+				tail = tail->next;
+			}
+			return ret;
+			
+		}
 
- private:
-  std::queue<T*> queue;
-  mythos::Mutex mutex;
+	private:
+			std::atomic<T* > head;
+			T* tail; 
 };
+
